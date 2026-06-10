868a1431a7760dce154fe8786613f26242d996ca
kumarDharmendra07 <dk4002732@gmail.com>
2026-06-10 23:08:06 +0530

    utils creation

--- src/index.js
+++ src/index.js
@@ -3,3 +3,17 @@ import connectDB from './db/dataBase.js'
 
 
 connectDB()
+.then(() => {
+
+    app.on("error", (error) => {
+    console.log("Error : ",error.message)
+    throw error
+    })
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("Database connection error : ", err.message)    
+})
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+
+    //using try catch
+const asyncHandler = (controllerFunction) => async (req, res, next) => {
+    try {
+        await controllerFunction(req, res, next)
+    } catch (error) {
+        res.status(error.code || 500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+    //using promises
+// const asyncHandler = (requestHandler) => {
+//     (req, res, next) => {
+//         Promise.resolve(requestHandler(req, res, next))
+//         .catch((error) => next(error))
+//     }
+// } 
+
+export { asyncHandler }
